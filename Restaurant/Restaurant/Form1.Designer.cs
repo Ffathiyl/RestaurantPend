26d58f9bd9f145fac18ad0201c72a0dc0487e572
virya <136900881+idknmn@users.noreply.github.com>
2024-05-10 20:43:48 +0700

    Tes

--- Restaurant/Restaurant/Form1.Designer.cs
+++ Restaurant/Restaurant/Form1.Designer.cs
@@ -28,21 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txt_Username = new System.Windows.Forms.TextBox();
+            this.txt_Password = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // txt_Username
+            // 
+            this.txt_Username.Location = new System.Drawing.Point(359, 140);
+            this.txt_Username.Name = "txt_Username";
+            this.txt_Username.Size = new System.Drawing.Size(100, 20);
+            this.txt_Username.TabIndex = 0;
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.Location = new System.Drawing.Point(359, 183);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.Size = new System.Drawing.Size(100, 20);
+            this.txt_Password.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_Password);
+            this.Controls.Add(this.txt_Username);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txt_Username;
+        private System.Windows.Forms.TextBox txt_Password;
     }
 }
 
